3c547b8b5f001278b96f9584303f566bba37382c
Rootul Patel <rootulp@gmail.com>
2014-12-25 05:22:25 -0500

    functional pomodoro countdown

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+$.getScript("bower_components/FlipClock/compiled/flipclock.js", function(){
+  
+   var clock;
+   $(document).ready(function() {
+       
+       clock = $('.clock').FlipClock(1500, {
+           clockFace: 'MinuteCounter',
+           countdown: true,
+           autoStart: false,
+           callbacks: {
+               start: function() {
+                   $('.start').hide();
+               }
+           }
+       });
+
+       $('.start').click(function(e) {
+           clock.start();
+       });
+   });
+});
